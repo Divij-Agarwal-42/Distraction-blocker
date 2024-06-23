d727f51a2e33fd6bdd884e2fd940550f1269398c
Divij-Agarwal-42 <divij18a@gmail.com>
2024-06-23 20:04:58 +0800

    Playlists are not hidden now when recommended elements are hidden! + some code clean

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 
-// toggle1 is whether the recommendations need to be hidden or not
+// toggle1 is whether the recommendations need to be hidden or not, true means they need to be hidden
 // toggle2 is the same as toggle1 but for comments
 
 let toggle1 = true;
@@ -15,7 +15,7 @@ function reload_home() {
   }
 }
 
-// Hide recommendations and comments
+// function to hide recommendations and comments
 var justDoIt = function (){
 
   let noti_element = document.querySelector(".notification-button-style-type-default");
@@ -24,7 +24,7 @@ var justDoIt = function (){
     noti_element.remove()
   }
 
-  // Checking if the url
+  // Checking if the user is on YouTube's home page
   if ((window.location.href.indexOf("watch?v=") < 0) &&
       ((window.location.href.endsWith("youtube.com/")) || (window.location.href == "https://www.youtube.com") ||
       window.location.href.startsWith("https://www.youtube.com/?bp"))) {
@@ -44,16 +44,16 @@ var justDoIt = function (){
 
         if(toggle1){
           primary.style.display = "none";
-            console.log("This is working as expecte")
-            //recommended_element.style.display = "none";
         }
         if(side_menu && toggle1){
             side_menu.style.display = "none";
         }
     }
 
-  } else {
-    let related_element = document.querySelector("#related")
+  } else { // This means that the user is watching a video
+
+    //related_element refers to the recommended videos shown on the side
+    let related_element = document.querySelector('#related').querySelector("#items");
     let comments_element = document.querySelector("#comments")
 
     if (related_element != null && comments_element != null) {
@@ -72,7 +72,7 @@ var justDoIt = function (){
 }
 
 let interval_id = null;
-// Load values for hide recommendations, hide comments
+// Load settings for whether recommendations and comments need to be hidden
 async function load_values() {
   try {
     let hidden_recs = await chrome.storage.local.get(["hide_recs"]);
@@ -80,10 +80,10 @@ async function load_values() {
     toggle1 = hidden_recs.hide_recs;
     toggle2 = hidden_comms.hide_coms;
     console.log(toggle1, toggle2);
-    interval_id = setInterval(function () {justDoIt()}, 100);
-  } catch {
 
-  }
+    // repeatedly tries to find elements every 100 ms to hide them
+    interval_id = setInterval(function () {justDoIt()}, 100);
+  } catch {}
 };
 load_values();
 
