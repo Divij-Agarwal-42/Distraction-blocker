52f11a6b870fd575bac516f837e965fa8c2a904c
Divij-Agarwal-42 <divij18a@gmail.com>
2024-11-30 12:34:14 -0500

    popup.js code cleaning / code standards

--- popup.js
+++ popup.js
@@ -1,4 +1,25 @@
+/*
+
+This script is activated when user clicks the extension icon.
+
+Code that's run automatically:
+Checks whether any previous breaks are going on (manages styles for popup + logic accordingly)
+Checks for the previous saved break time and then loads it up for displaying on front end
+
+Other stuff in the script:
+Break timer logic is there, taking inputs from user to start break etc
+
+*/
+
 import { set_break_settings, get_break_settings } from "./background.js";
+
+const ONE_HOUR = 3600;
+const ONE_MINUTE = 60;
+const ONE_SECOND = 1000;
+
+const BREAK_NOT_STARTED = 0;
+const BREAK_STARTED = 1;
+const BREAK_ONGOING = 2
 
 let time_value = document.getElementById("timeInput").value; // Time input field value
 let break_time_value = 0; // The current value of break time in minutes
@@ -12,21 +33,22 @@ let timerInterval;
 timeSettings.style.visibility = "hidden";
 quitYt.style.visibility = "hidden";
 
+// Loading prev saved break time for front end
 function currentTimeText(new_time) {
   currentTime.innerText = "Break settings will be applied for: " + new_time + " min";
   break_time_value = Number(new_time);
 }
-
 chrome.storage.local.get().then(breakSettings => {
   currentTimeText(breakSettings.break_time);
 });
 
 function calculateTimeDifferenceInSeconds(date1, date2) {
   const differenceInMilliseconds = date1.getTime() - date2.getTime();
-  const differenceInSeconds = Math.floor(differenceInMilliseconds / 1000);
+  const differenceInSeconds = Math.floor(differenceInMilliseconds / ONE_SECOND);
   return differenceInSeconds;
 }
 
+// Fetches break's start time and current time. Then returns difference
 function checkRemainingTime() {
   const currentDate = new Date();
   const startDate = new Date(localStorage.getItem("start time"));
@@ -36,10 +58,10 @@ function checkRemainingTime() {
 }
 
 function formatTime(seconds) {
-  const hours = Math.floor(seconds / 3600);
-  const remainingSeconds = seconds % 3600;
-  const minutes = Math.floor(remainingSeconds / 60);
-  const seconds2 = seconds - (minutes * 60) - (hours * 3600);
+  const hours = Math.floor(seconds / ONE_HOUR);
+  const remainingSeconds = seconds % ONE_HOUR;
+  const minutes = Math.floor(remainingSeconds / ONE_MINUTE);
+  const seconds2 = seconds - (minutes * ONE_MINUTE) - (hours * ONE_HOUR);
   
   const formattedHours = String(hours).padStart(2, '0');
   const formattedMinutes = String(minutes).padStart(2, '0');
@@ -49,11 +71,12 @@ function formatTime(seconds) {
 }
 
 function updateTimer(dontReduceTime = false) {
-  if (remainingTime > 3600) {
+  // Note: remaining time is the time remaining for break
+  if (remainingTime > ONE_HOUR) {
     get_break_time = formatTime(remainingTime);
   } else {
-    const minutes = Math.floor(remainingTime / 60);
-    const seconds = remainingTime % 60;
+    const minutes = Math.floor(remainingTime / ONE_MINUTE);
+    const seconds = remainingTime % ONE_MINUTE;
     get_break_time = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
   }
   document.getElementById("break").innerText = get_break_time;
@@ -67,27 +90,28 @@ function updateTimer(dontReduceTime = false) {
     // Break over, removing stored data
     localStorage.removeItem("start time");
     initial_break_style();
-    breakStatus = 0;
+    breakStatus = BREAK_NOT_STARTED;
   }
 }
 
 
 // On start of popup, checks whether break is ongoing or not
+// If ongoing, starts timer by calling updateTimer every second
 chrome.storage.local.get().then(breakSettings => {
   console.log("ongoing value, popup: ", breakSettings.ongoing);
   if (breakSettings.ongoing) {
-    if (checkRemainingTime() < (breakSettings.break_time * 60)) {
-      breakStatus = 2;
-      remainingTime = (breakSettings.break_time * 60) - checkRemainingTime();
+    if (checkRemainingTime() < (breakSettings.break_time * ONE_MINUTE)) {
+      breakStatus = BREAK_ONGOING;
+      remainingTime = (breakSettings.break_time * ONE_MINUTE) - checkRemainingTime();
       updateTimer();
 
-      console.log("break time value: ", breakSettings.break_time);
-      console.log("check remaining time function", checkRemainingTime())
-      console.log("remaining time", remainingTime);
+      // console.log("break time value: ", breakSettings.break_time);
+      // console.log("check remaining time function", checkRemainingTime())
+      // console.log("remaining time", remainingTime);
 
       document.getElementById("break").style.backgroundColor = "#B0301F"
       document.getElementById("break").style.color = "white";
-      timerInterval = setInterval(updateTimer, 1000);
+      timerInterval = setInterval(updateTimer, ONE_SECOND);
     } else {
       // Break over, removing stored data
       localStorage.removeItem("start time");
@@ -95,6 +119,7 @@ chrome.storage.local.get().then(breakSettings => {
   }
 });
 
+// Handling input for break time
 document.getElementById("timeInput").addEventListener("input", function (event) {
   let input = event.target.value;
 
@@ -110,6 +135,7 @@ document.getElementById("timeInput").addEventListener("input", function (event) 
 
 })
 
+// Handling "user clicked submit icon" for new break time
 document.getElementById("submitTime").addEventListener("click", function (event) {
   time_value = document.getElementById("timeInput").value;
   if (time_value == "" || time_value == null) {
@@ -127,36 +153,26 @@ function initial_break_style() {
   document.getElementById("break").style.color = "black";
 }
 
+// Handles the styles of the popup's front end depending on if break is ongoing or nah
 document.getElementById("break").addEventListener("click", async function (event) {
   let timeSettings = document.getElementById("timeSettings");
 
-  if (breakStatus == 0) { // Break is going to start, settings are being selected
+  if (breakStatus == BREAK_NOT_STARTED) {
     initial_break_style();
-    breakStatus = 1;
+    breakStatus = BREAK_STARTED;
 
-  } else if (breakStatus == 1) { // Break is starting
+  } else if (breakStatus == BREAK_STARTED) {
     timeSettings.style.visibility = "hidden";
     quitYt.style.visibility = "hidden";
     await set_break_settings(break_time_value, document.getElementById("quitInput").checked, true);
 
     document.getElementById("break").style.backgroundColor = "#B0301F";
     document.getElementById("break").style.color = "white";
-    breakStatus = 2;
+    breakStatus = BREAK_ONGOING;
     chrome.runtime.sendMessage({action: "start", value: break_time_value});
     localStorage.setItem("start time", new Date());
-    remainingTime = break_time_value * 60;
-    timerInterval = setInterval(updateTimer, 1000); // Update every second    
+    remainingTime = break_time_value * ONE_MINUTE;
+    timerInterval = setInterval(updateTimer, ONE_SECOND); // Update every second    
 
   }
 })
-
-// chrome.runtime.sendMessage({ action: "getTime" }, function(response) {
-//   get_break_time = response;
-//   console.log(get_break_time);
-
-//   if ((get_break_time != "00:00")) {
-//     console.log("Hi/???")
-//     breakStatus = 2;
-//     const timerInterval = setInterval(updateTimer, 1000); // Update every second      
-//   }
-// });
